268f708d8fdf288112a005b28c2bf2b891cf2e6b
juangh15 <47037988+juangh15@users.noreply.github.com>
2019-07-28 10:55:13 -0500

    Bases

--- src/com/ViasApp/inmovil/GrafoVias.scala
+++ src/com/ViasApp/inmovil/GrafoVias.scala
@@ -1,5 +1,21 @@
-package com.ViasApp.inmovil
+package inmovil
+import com.ViasApp.movimiento._
+import scalax.collection.mutable.Graph
+import scalax.collection.edge.WLDiEdge
 
-class GrafoVias {
+object GrafoVias {
+  var _grafo = Graph[Interseccion, WLDiEdge]()
+  def construir(vias: Array[Via]){
+    vias.foreach(via_actual => {
+      
+      grafo.add(WLDiEdge(via_actual.origen, via_actual.fin)(via_actual.longitud, via_actual))
+      if (via_actual.sentido.nombre == "dobleVia"){
+        grafo.add(WLDiEdge(via_actual.fin, via_actual.origen)(via_actual.longitud, via_actual))
+      }
+    }
+  )
+  }
   
+  def grafo = _grafo //getter
+
 }
