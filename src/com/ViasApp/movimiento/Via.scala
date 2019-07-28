dd10032971b7f596f40ceb7746ca7e24bf7db16d
juangh15 <47037988+juangh15@users.noreply.github.com>
2019-07-28 11:03:35 -0500

    Bases

--- src/com/ViasApp/movimiento/Via.scala
+++ src/com/ViasApp/movimiento/Via.scala
@@ -1,9 +1,15 @@
 package com.ViasApp.movimiento
 import com.ViasApp.inmovil.Recta
 
-class Via(var or: Interseccion, var fn: Interseccion) extends Recta {
+class Via(var or: Interseccion, var fn: Interseccion, val velocidad: Int, val tipovia: TipoVia, val sentido: Sentido, val numero: String, val nombre: String) extends Recta {
   type Origen = Interseccion
   type Fin = Interseccion
-  var origen: Origen = or
-  var fin: Fin = fn
+  var _origen: Origen = or
+  var _fin: Fin = fn
+  val longitud = math.sqrt(math.pow(origen.x - fin.x, 2) + math.pow(origen.y - fin.y, 2)).toInt
+  
+  def origen = _origen  //getters
+  def fin = _fin
+  
+  override def toString = s"Via: ${numero} con ${nombre} "
 }
