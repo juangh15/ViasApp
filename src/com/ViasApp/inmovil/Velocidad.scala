ba721ff1b3d82392b249fdcacb2bec7b71967f7d
juangh15 <47037988+juangh15@users.noreply.github.com>
2019-07-28 10:57:45 -0500

    Bases

--- src/com/ViasApp/inmovil/Velocidad.scala
+++ src/com/ViasApp/inmovil/Velocidad.scala
@@ -1,5 +1,10 @@
 package com.ViasApp.inmovil
 
-class Velocidad {
+class Velocidad(private var _magnitud: Double, private var _direccion: Angulo) {
   
+  def magnitud = _magnitud
+  def direccion = _direccion
+  
+  def magnitud_=(mag: Double): Unit = _magnitud = mag
+  def direccion_=(dir: Angulo): Unit = _direccion = dir
 }
