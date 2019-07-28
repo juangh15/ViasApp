43a03263fb59d92b36c07bb92c2baf1e0c91d3a5
juangh15 <47037988+juangh15@users.noreply.github.com>
2019-07-28 11:00:15 -0500

    Bases

--- src/com/ViasApp/movil/Movil.scala
+++ src/com/ViasApp/movil/Movil.scala
@@ -1,5 +1,11 @@
 package com.ViasApp.movil
+import com.ViasApp.inmovil._
 
-class Movil {
+abstract class Movil(var _posicion: Punto, var _velocidad: Velocidad) {
   
+  
+  def posicion = _posicion
+  def velocidad = _velocidad
+  
+  def mover(dt: Double)
 }
