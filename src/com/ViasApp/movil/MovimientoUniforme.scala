8bcee78b24d266a715cb7cf1257efa0c818bb6c9
juangh15 <47037988+juangh15@users.noreply.github.com>
2019-07-28 11:00:58 -0500

    Bases

--- src/com/ViasApp/movil/MovimientoUniforme.scala
+++ src/com/ViasApp/movil/MovimientoUniforme.scala
@@ -1,5 +1,12 @@
 package com.ViasApp.movil
 
-trait MovimientoUniforme {
-  
+trait MovimientoUniforme extends Movil {
+  def mover(dt: Double){
+   val dx = velocidad.magnitud * math.cos(velocidad.direccion.valor) * dt
+   val dy = velocidad.magnitud * math.sin(velocidad.direccion.valor) * dt
+   val mueveX = posicion.x + dx
+   val mueveY = posicion.y + dy
+   posicion.x_=(mueveX)
+   posicion.y_=(mueveY)
+  } 
 }
