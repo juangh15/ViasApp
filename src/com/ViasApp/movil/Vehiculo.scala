d952a50e9fa28d950f3b4b052a24dda52e890af0
juangh15 <47037988+juangh15@users.noreply.github.com>
2019-07-28 11:01:28 -0500

    Bases

--- src/com/ViasApp/movil/Vehiculo.scala
+++ src/com/ViasApp/movil/Vehiculo.scala
@@ -1,5 +1,6 @@
 package com.ViasApp.movil
+import com.ViasApp.inmovil._
 
-class Vehiculo {
-  
+abstract class Vehiculo(val placa: String,var pos: Punto,var vel: Velocidad) extends Movil(pos, vel) with MovimientoUniforme {
+  override def toString = s"${this.getClass}(placa=${placa}, posicion=($posicion))"
 }
