08ea3eb7901c225be6a64db6f47e64c04e3226d4
darstih <48297481+darstih@users.noreply.github.com>
2019-07-31 08:19:09 -0500

    Update Simulacion.scala

--- src/com/ViasApp/inmovil/Simulacion.scala
+++ src/com/ViasApp/inmovil/Simulacion.scala
@@ -6,8 +6,15 @@ import com.ViasApp.movil._
 
 object Simulacion  extends Runnable {
   
-  val grafico = new Grafico
   var t : Int = 0
+  
+  
+
+  
+  
+  
+  
+  
   
   // lectura variables del json
   val json = new Json
@@ -18,13 +25,38 @@ object Simulacion  extends Runnable {
   val minVelocidad = json.minVelocidad
   val maxVelocidad = json.maxVelocidad
   
+  val totalVehiculos = (math.random()*(maxVehiculos-minVehiculos))+minVehiculos
+  val carros = math.random()*totalVehiculos
+  val motos = math.random()*(totalVehiculos-carros)+carros
+  //Así hasta motoTaxis
+  val r.scala.util.Random
+  var inicio = new Punto()
+  var final = new Punto()
+  for(i <- carros){
+    //inicio=//Aqui tiene que darle la interseccion de origen
+    //final= //Aqui le tiene que asignar la final
+    
+    //Tiene que generar la lista que llegue más rapido al final de cada carro y no se si guardarlo en una matriz
+    
+    vehiculos += new Carro(s"${r.nextPrintableChar}${r.nextPrintableChar}${r.nextPrintableChar}${r.nextInt}${r.nextInt}${r.nextInt}",(math.random()*(maxVelocidad-minVelocidad)+minVelocidad),inicio)
+  }
+  //Así hasta moto taxi
+
+    
+    var g = GrafoVias.construir(vias)//Devuelve graph del mapa
+    val grafico = new Grafico()//Tal vez este mal escrito
+    val ventana = grafico.graficarVias(vias) //Devuelve un Jframe con el dibujo del mapa(JFrame es una ventana que contiene dibujo titulo y otras cosas)
+
+    //val chart = ventana.(fnqueextraeelfreechart)
+
+    val plot: XYPlot = ventana.getXYPlot()//Si da error hay que buscar una funcion que extraiga el Jfreechart
   
   
  def run() {
    while (true) {
      vehiculos.foreach(_.mover(dt))
      t += dt
-     grafico.graficarVehiculos(vehiculos)
+     val imagen = grafico.graficarVehiculos(vehiculos)
      Thread.sleep(tRefresh)
  }
 }
