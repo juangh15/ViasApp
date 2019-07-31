84463d53b627fb95b181100a1aea1bb46356f2cb
ALEJOMO23 <53061178+ALEJOMO23@users.noreply.github.com>
2019-07-30 22:08:23 -0500

    Inicios clase Simulación
    se copia el metodo run e inicializan valores de la futura clase Json

--- src/com/ViasApp/inmovil/Simulacion.scala
+++ src/com/ViasApp/inmovil/Simulacion.scala
@@ -1,7 +1,31 @@
-package com.ViasApp.inmovil
+package com.ViasApp.main
+import scala.collection.mutable.ArrayBuffer
+import com.ViasApp.movimiento._
+import com.ViasApp.inmovil._
+import com.ViasApp.movil._
 
-class Simulacion extends Runnable {
-  def run{
-    
-  }
+object Simulacion  extends Runnable {
+  
+  val grafico = new Grafico
+  var t : Int = 0
+  
+  // lectura variables del json
+  val json = new Json
+  val dt = json.dt
+  val tRefresh = json.tRefresh
+  val minVehiculos = json.minVehiculos
+  val maxVehiculos = json.maxVehiculos
+  val minVelocidad = json.minVelocidad
+  val maxVelocidad = json.maxVelocidad
+  
+  
+  
+ def run() {
+   while (true) {
+     vehiculos.foreach(_.mover(dt))
+     t += dt
+     grafico.graficarVehiculos(vehiculos)
+     Thread.sleep(tRefresh)
+ }
+}
 }
