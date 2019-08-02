963d3c27e64e6145c8681d0447de69811d9097f5
juangh15 <47037988+juangh15@users.noreply.github.com>
2019-08-02 00:15:12 -0500

    TrazarRuta

--- src/com/ViasApp/inmovil/GrafoVias.scala
+++ src/com/ViasApp/inmovil/GrafoVias.scala
@@ -4,18 +4,28 @@ import scalax.collection.mutable.Graph
 import scalax.collection.edge.WLDiEdge
 
 object GrafoVias {
-  var _grafo = Graph[Interseccion, WLDiEdge]()
-  def construir(vias: Array[Via]){
-    vias.foreach(via_actual => {
-      
-      grafo.add(WLDiEdge(via_actual.origen, via_actual.fin)(via_actual.longitud, via_actual))
-      if (via_actual.sentido.nombre == "dobleVia"){
-        grafo.add(WLDiEdge(via_actual.fin, via_actual.origen)(via_actual.longitud, via_actual))
-      }
-    }
-  )
-  }
   
+  var _grafo = Graph[Interseccion, WLDiEdge]()
   def grafo = _grafo //getter
 
+
+  def construir(vias: Array[Via]) {
+    vias.foreach(via_actual => {
+
+      grafo.add(WLDiEdge(via_actual.origen, via_actual.fin)(via_actual.longitud, via_actual))
+      if (via_actual.sentido.nombre == "dobleVia") {
+        grafo.add(WLDiEdge(via_actual.fin, via_actual.origen)(via_actual.longitud, via_actual))
+      }
+    })
+  }
+
+  def trazarRuta(inicio: Interseccion, fin: Interseccion, k: Graph[Interseccion, WLDiEdge]=GrafoVias.grafo): k.Path = {
+    def nodo(outer: Interseccion): k.NodeT = k get outer
+
+    val n1: k.NodeT = nodo(inicio)
+    val n2: k.NodeT = nodo(fin)
+    val z = (n1) shortestPathTo (n2)
+    return z.get
+
+  }
 }
