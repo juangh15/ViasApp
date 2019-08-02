4c97e46ab0bd7ccb6e7f74e94ca2c511cca29cd4
ALEJOMO23 <53061178+ALEJOMO23@users.noreply.github.com>
2019-08-01 23:41:03 -0500

    case
    se cambia clase a case class

--- src/com/ViasApp/movimiento/Interseccion.scala
+++ src/com/ViasApp/movimiento/Interseccion.scala
@@ -1,7 +1,7 @@
 package com.ViasApp.movimiento
 import com.ViasApp.inmovil.Punto
 
-class Interseccion(private var _x: Int, private var _y: Int, val nombre: String = "SinNombre") extends Punto(_x,_y){
+case class Interseccion(private var _x: Int, private var _y: Int, val nombre: String = "SinNombre") extends Punto(_x,_y){
   
   override def toString = s"Inters: ${nombre}"
 }
