0202c96b08da4b1e38f5667f9f6d9501659128cf
darstih <48297481+darstih@users.noreply.github.com>
2019-08-02 23:56:02 -0500

    Update Via.scala

--- src/com/ViasApp/movimiento/Via.scala
+++ src/com/ViasApp/movimiento/Via.scala
@@ -10,6 +10,31 @@ class Via(var or: Interseccion, var fn: Interseccion, val velocidad: Int, val ti
   
   def origen = _origen  //getters
   def fin = _fin
-  
+    def angulo = new Angulo ({
+    val m=(fin.y - origen.y)/(fin.x-origen.x)
+    var res : Double = 0
+    if(m<0){
+      if(fin.y>origen.y){
+        if(fin.x<origen.x) {
+          res=Math.tan(m)+Math.toRadians(90)
+        } else {
+          res=Math.tan(m)-Math.toRadians(90)
+        }
+      }
+      
+    }else if(m>0){
+      if(fin.y<origen.y){
+        if(fin.x<origen.x) {res=Math.tan(m)+Math.toRadians(180)} else {res=Math.tan(m)}
+      }
+    }
+    
+    if(fin.y==origen.y){
+      if(fin.x>origen.x) {res=Math.toRadians(0)} else {res=Math.toRadians(180)}
+    }
+    if(fin.x==origen.x){
+      if(fin.y>origen.y) {res=Math.toRadians(90)} else {res=Math.toRadians(270)}
+    }
+    res
+    })
   override def toString = s"Via: ${numero} con ${nombre} "
 }
