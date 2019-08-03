f2148d9058a401c3ff17270ae75f6eee39bc511a
juangh15 <47037988+juangh15@users.noreply.github.com>
2019-08-03 00:13:40 -0500

    getset velocidad

--- src/com/ViasApp/movil/Movil.scala
+++ src/com/ViasApp/movil/Movil.scala
@@ -6,6 +6,7 @@ abstract class Movil(var _posicion: Punto, var _velocidad: Velocidad) {
   
   def posicion = _posicion
   def velocidad = _velocidad
-  
+  def posicion_=(posicion_nuevo:Punto): Unit = _posicion = posicion_nuevo
+  def velocidad_=(velocidad_nuevo:Velocidad): Unit = _velocidad = velocidad_nuevo
   def mover(dt: Double)
 }
