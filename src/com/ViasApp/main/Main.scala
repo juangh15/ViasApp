3a1de2afd33936fa3455e308dc7610e7761d6b7e
juangh15 <47037988+juangh15@users.noreply.github.com>
2019-08-25 10:43:55 -0500

    rutas JSON

--- src/com/ViasApp/main/Main.scala
+++ src/com/ViasApp/main/Main.scala
@@ -3,12 +3,17 @@ import com.ViasApp.movimiento._
 import scala.collection.mutable.Queue
 import com.ViasApp.inmovil._
 import com.ViasApp.movil._
+import com.ViasApp.JsonClasses._
 import scala.io.StdIn.readLine
 
-
 object Main {
+  
+  //ruta donde se CARGARAN los parametros de la simulacion (DEBE EXISTIR "parametros.json" PARA EJECUTAR CORRECTAMENTE)
+  val rutaParametros = "C:\\Users\\....\\Desktop\\ViasApp-master\\resources\\parametros.json"
+  //ruta donde se GUARDARAN los parametros de la simulacion
+  val rutaResultados = "C:\\Users\\....\\Desktop\\ViasApp-master\\resources\\resultados.json"
+  
   def main(args: Array[String]): Unit = {
-   Simulacion.run()
+    Simulacion.run()
   }
-
 }
