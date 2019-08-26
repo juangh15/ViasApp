1dc7e4738b0e3869987a52eb9438195002ef05b6
juangh15 <47037988+juangh15@users.noreply.github.com>
2019-08-25 20:07:53 -0500

    Option nombre

--- src/com/ViasApp/movimiento/Interseccion.scala
+++ src/com/ViasApp/movimiento/Interseccion.scala
@@ -1,7 +1,7 @@
 package com.ViasApp.movimiento
 import com.ViasApp.inmovil.Punto
 
-case class Interseccion(private var _x: Int, private var _y: Int, val nombre: String = "SinNombre") extends Punto(_x,_y){
+case class Interseccion(private var _x: Double, private var _y: Double, val  nombre: Option[String]=None) extends Punto(_x,_y){
   
   override def toString = s"Inters: ${nombre}"
 }
