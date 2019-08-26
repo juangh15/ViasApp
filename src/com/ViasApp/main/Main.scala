047c08c7dc9a7cfc521918374d8f46bb458ea732
juangh15 <47037988+juangh15@users.noreply.github.com>
2019-08-25 20:04:45 -0500

    user n password neo4j

--- src/com/ViasApp/main/Main.scala
+++ src/com/ViasApp/main/Main.scala
@@ -1,18 +1,21 @@
 package com.ViasApp.main
 import com.ViasApp.movimiento._
-import scala.collection.mutable.Queue
 import com.ViasApp.inmovil._
 import com.ViasApp.movil._
-import com.ViasApp.JsonClasses._
-import scala.io.StdIn.readLine
 
 object Main {
-  
+
   //ruta donde se CARGARAN los parametros de la simulacion (DEBE EXISTIR "parametros.json" PARA EJECUTAR CORRECTAMENTE)
-  val rutaParametros = "C:\\Users\\....\\Desktop\\ViasApp-master\\resources\\parametros.json"
-  //ruta donde se GUARDARAN los parametros de la simulacion
-  val rutaResultados = "C:\\Users\\....\\Desktop\\ViasApp-master\\resources\\resultados.json"
+  val rutaParametros = "C:\\Users\\JuanG\\Desktop\\ViasApp-master\\resources\\parametros.json"
   
+  //ruta donde se GUARDARAN los parametros de la simulacion
+  val rutaResultados = "C:\\Users\\JuanG\\Desktop\\ViasApp-master\\resources\\resultados.json"
+
+  //URL, NOMBRE DE USUARIO Y CONTRASEÑA DE LA BASE DE DATOS EN NEO4J (DEBE ESTAR ACTIVA):
+  val url = "bolt://localhost/7687"
+  val user = "neo4j" //Usuario por defecto
+  val pass = "1234" //Pass de la bd activa
+
   def main(args: Array[String]): Unit = {
     Simulacion.run()
   }
