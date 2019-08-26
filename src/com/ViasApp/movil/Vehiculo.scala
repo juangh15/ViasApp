6592ff8cb3ae63ea0ff4e2c89a8eec3f0474caaa
darstih <48297481+darstih@users.noreply.github.com>
2019-08-26 17:05:24 -0500

    Agrego aceleracion y velocidadAuto
    Velocidad auto es la velocidad asignada a cada vehiculo en un momento inicial y es de tipo Double

--- src/com/ViasApp/movil/Vehiculo.scala
+++ src/com/ViasApp/movil/Vehiculo.scala
@@ -1,6 +1,6 @@
 package com.ViasApp.movil
 import com.ViasApp.inmovil._
 
-abstract class Vehiculo(val placa: String,var pos: Punto,var vel: Velocidad) extends Movil(pos, vel) with MovimientoUniformementeAcelerado {
+abstract class Vehiculo(val placa: String,var pos: Punto,var vel: Velocidad,val velocidadAuto:Velocidad, val ac: Int) extends Movil(pos, vel,ac) with MovimientoUniformementeAcelerado {
   override def toString = s"${this.getClass}(placa=${placa}, posicion=($posicion))"
 }
