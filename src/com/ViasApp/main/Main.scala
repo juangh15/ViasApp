9f34f582ab2746160ab356884dcd9c5c78bec2ad
juangh15 <47037988+juangh15@users.noreply.github.com>
2019-08-25 20:17:25 -0500

    ruta

--- src/com/ViasApp/main/Main.scala
+++ src/com/ViasApp/main/Main.scala
@@ -6,10 +6,10 @@ import com.ViasApp.movil._
 object Main {
 
   //ruta donde se CARGARAN los parametros de la simulacion (DEBE EXISTIR "parametros.json" PARA EJECUTAR CORRECTAMENTE)
-  val rutaParametros = "C:\\Users\\JuanG\\Desktop\\ViasApp-master\\resources\\parametros.json"
+  val rutaParametros = "C:\\Users\\...\\Desktop\\ViasApp-master\\resources\\parametros.json"
   
   //ruta donde se GUARDARAN los parametros de la simulacion
-  val rutaResultados = "C:\\Users\\JuanG\\Desktop\\ViasApp-master\\resources\\resultados.json"
+  val rutaResultados = "C:\\Users\\...\\Desktop\\ViasApp-master\\resources\\resultados.json"
 
   //URL, NOMBRE DE USUARIO Y CONTRASEÑA DE LA BASE DE DATOS EN NEO4J (DEBE ESTAR ACTIVA):
   val url = "bolt://localhost/7687"
