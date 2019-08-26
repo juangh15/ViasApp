d2c96f8cf01133c0e1bfbde73d287337a52077d8
darstih <48297481+darstih@users.noreply.github.com>
2019-08-26 17:02:37 -0500

    agregando aceleración

--- src/com/ViasApp/movil/Movil.scala
+++ src/com/ViasApp/movil/Movil.scala
@@ -1,12 +1,7 @@
 package com.ViasApp.movil
 import com.ViasApp.inmovil._
 
-abstract class Movil(var _posicion: Punto, var _velocidad: Velocidad) {
+abstract class Movil(var posicion: Punto, var velocidad: Velocidad, var aceleracion: Double) {
   
-  
-  def posicion = _posicion
-  def velocidad = _velocidad
-  def posicion_=(posicion_nuevo:Punto): Unit = _posicion = posicion_nuevo
-  def velocidad_=(velocidad_nuevo:Velocidad): Unit = _velocidad = velocidad_nuevo
   def mover(dt: Double)
 }
