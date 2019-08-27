8be687cfb484549dec9b883e205d98c04abf5241
darstih <48297481+darstih@users.noreply.github.com>
2019-08-27 02:39:09 -0500

    Update Movil.scala

--- src/com/ViasApp/movil/Movil.scala
+++ src/com/ViasApp/movil/Movil.scala
@@ -1,7 +1,7 @@
 package com.ViasApp.movil
 import com.ViasApp.inmovil._
 
-abstract class Movil(var posicion: Punto, var velocidad: Velocidad, var aceleracion: Double) {
+abstract class Movil(var posicion: Punto, var velocidad: Velocidad, var aceleracion: Double, val va:Double) {
   
   def mover(dt: Double, acelera:Boolean)
   def distanciaFrenado
