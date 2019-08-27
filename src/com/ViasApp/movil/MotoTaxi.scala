7feb059e3c74f06c830e611ac66f44e02fe6a18c
darstih <48297481+darstih@users.noreply.github.com>
2019-08-27 00:23:02 -0500

    Update MotoTaxi.scala

--- src/com/ViasApp/movil/MotoTaxi.scala
+++ src/com/ViasApp/movil/MotoTaxi.scala
@@ -1,6 +1,4 @@
 package com.ViasApp.movil
 import com.ViasApp.inmovil._
 
-class MotoTaxi(val pla: String="SinPlacas", var po: Punto = new Punto(0,0), var ve: Velocidad = new Velocidad(1, new Angulo(0))) extends Vehiculo(pla, po, ve){
-  
-}
+class MotoTaxi(val pla: String="SinPlacas", var po: Punto = new Punto(0,0), var ve: Velocidad = new Velocidad(1, new Angulo(0)), va:Double , ac:Int=0) extends Vehiculo(pla, po, ve,va ,ac)
