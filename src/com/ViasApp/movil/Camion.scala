df846396dd817d9a74d4c2f4571d7bfff510368b
darstih <48297481+darstih@users.noreply.github.com>
2019-08-27 00:19:38 -0500

    Update Camion.scala

--- src/com/ViasApp/movil/Camion.scala
+++ src/com/ViasApp/movil/Camion.scala
@@ -1,6 +1,6 @@
 package com.ViasApp.movil
 import com.ViasApp.inmovil._
 
-class Camion(val pla: String="SinPlacas", var po: Punto = new Punto(0,0), var ve: Velocidad = new Velocidad(1, new Angulo(0))) extends Vehiculo(pla, po, ve){
+class Camion(val pla: String="SinPlacas", var po: Punto = new Punto(0,0), var ve: Velocidad = new Velocidad(1, new Angulo(0)),val va:Double , val ac=Double) extends Vehiculo(pla, po, ve,va, ac){
   
 }
