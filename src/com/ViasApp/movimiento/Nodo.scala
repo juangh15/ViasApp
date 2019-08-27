cf9fa6a0c153bbacc9fde57532f82eb123ed886e
darstih <48297481+darstih@users.noreply.github.com>
2019-08-27 04:04:04 -0500

    semaforos
    Ya alterna los semaforos dependiendo del tiempo en el que este

--- src/com/ViasApp/movimiento/Nodo.scala
+++ src/com/ViasApp/movimiento/Nodo.scala
@@ -1,22 +1,61 @@
 package com.ViasApp.movimiento
 
+import scala.collection.mutable.ArrayBuffer
 
 
-class Nodo(val id: Int,val semaforos: Array[Option[Semaforo]], var funcionar:Boolean=true ) {
+
+class Nodo(val id: Int,val semaforos: ArrayBuffer[Semaforo], var funcionar:Boolean=true ) {
     //Tiempo en amarillo es estandar para todos
-  val ta:Double=2//nùmero estandar
-  val semaforosExistentes=semaforos.flatten
+  var tiempo:Int=0
+  val ta:Int=2//nùmero estandar
   
-  //hay secuencia.length() -1 semaforos
+  val tiempoTotal = {var ñ=0;for(i <- semaforos){ñ=ñ+i.tv};ñ}+ta*(semaforos.length)
   
   
-  def procesoDeCambio(x:Array[Semaforo], i:Int){
-    x(i-1).estado=1
-    
-    //luego 
-    x(i-1).estado=2
-    x(i).estado=1
+  //estados iniciales
+  for(i <- semaforos){
+    i.estado=2
   }
+  semaforos(0).estado=0
   
+  
+  
+  def procesoDeCambioSemaforos(dt:Int){
+    tiempo+=dt
+    if(tiempo>=tiempoTotal){
+      tiempo=tiempo-tiempoTotal
+    }
+    
+    var sumatory:Int=semaforos(0).tv
+    var condicion =(tiempo<=sumatory)
+    var cont=0
+    var cosa=false
+    while(!condicion){
+      
+      if(cosa){
+        sumatory+=ta
+        cosa=false
+        cont+=1
+      }else{
+        sumatory+=semaforos(cont).tv
+        cosa=true
+      }
+      condicion=(tiempo<=sumatory)      
+    }
+    
+    if(cosa){
+      semaforos(cont).estado=1
+    }else{
+      semaforos(cont).estado=0
+      if(!(cont-1<0)){
+        semaforos(cont-1).estado=2
+      }else {
+        semaforos(semaforos.length-1).estado=2
+      }
+      
+    }
+    
+      
+  }
   
 }
