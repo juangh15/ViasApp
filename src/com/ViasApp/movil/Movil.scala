ffbe12755e35c8846af2d04379c022095479a519
ALEJOMO23 <53061178+ALEJOMO23@users.noreply.github.com>
2019-08-27 14:30:39 -0500

    Corrige error del mixin
    Cambia el tipo de acelera por Int para arreglar el error del mixin

--- src/com/ViasApp/movil/Movil.scala
+++ src/com/ViasApp/movil/Movil.scala
@@ -3,6 +3,6 @@ import com.ViasApp.inmovil._
 
 abstract class Movil(var posicion: Punto, var velocidad: Velocidad, val velocidadAuto:Double, var aceleracion: Int) {
   
-  def mover(dt: Double, acelera:Boolean)
+  def mover(dt: Double, acelera: Int)
   def distanciaFrenado:Double
 }
