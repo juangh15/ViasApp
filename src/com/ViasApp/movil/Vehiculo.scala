c33a54f985cc6f8e78397841370862ebc1a0bfb6
juangh15 <47037988+juangh15@users.noreply.github.com>
2019-08-27 09:08:07 -0500

    orden en constructor

--- src/com/ViasApp/movil/Vehiculo.scala
+++ src/com/ViasApp/movil/Vehiculo.scala
@@ -1,6 +1,6 @@
 package com.ViasApp.movil
 import com.ViasApp.inmovil._
 
-abstract class Vehiculo(val placa: String,var pos: Punto,var vel: Velocidad,val velocidadAuto:Double, val ac: Int) extends Movil(pos, vel,ac,velocidadAuto) with MovimientoUniformementeAcelerado {
+abstract class Vehiculo(val placa: String,var pos: Punto,var vel: Velocidad, val va:Double,val ac: Int) extends Movil(pos, vel,va, ac) with MovimientoUniformementeAcelerado {
   override def toString = s"${this.getClass}(placa=${placa}, posicion=($posicion))"
 }
