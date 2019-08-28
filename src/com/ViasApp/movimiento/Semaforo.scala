949789e157180052874efa3703af80de13b81de1
ALEJOMO23 <53061178+ALEJOMO23@users.noreply.github.com>
2019-08-28 03:02:54 -0500

    Se actualiza la clase

--- src/com/ViasApp/movimiento/Semaforo.scala
+++ src/com/ViasApp/movimiento/Semaforo.scala
@@ -5,17 +5,24 @@ import com.ViasApp.inmovil.Velocidad
 
 //viaAsociada no sè si guardarla acà
 class Semaforo(val id:Long,var estado:Int, val tv:Double) {
-  //var nodo : Nodo
+  
+ // var nodo : Nodo
+  
   //Estado 0:Verde
   //Estado 1:Amarillo
   //estado 2:Rojo
-  
-
-  
+ 
   //tv si es variable
 
-  
-  
   //verdadero solo mientras creo esa vaina
-  def sePuedePasar(v:Double):Boolean=true
+  def sePuedePasar(v:Double, a:Double, d:Double = 0.0): Boolean = {
+    var t:Int=0
+    if(a==0){
+      t=(d/v).toInt
+    }else{
+      t=((-v+Math.sqrt(v*v+2*a*d))/a).toInt
+    }
+   // (nodo.tiempoSemaforo(id)>= t)
+    true
+  }
 }
