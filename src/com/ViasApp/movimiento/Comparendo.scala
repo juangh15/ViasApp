ef06ffbd34137fcb07e823b889964cdad0e7547d
juangh15 <47037988+juangh15@users.noreply.github.com>
2019-08-27 21:13:34 -0500

    case para comparendos

--- src/com/ViasApp/movimiento/Comparendo.scala
+++ src/com/ViasApp/movimiento/Comparendo.scala
@@ -1,4 +1,4 @@
 package com.ViasApp.movimiento
 import com.ViasApp.movil.Vehiculo
 
-class Comparendo(val vehiculo: Vehiculo, val velocidadVehiculo: Double, val velocidadVia: Double)
+case class Comparendo(val vehiculo: Vehiculo, val velocidadVehiculo: Double, val velocidadVia: Double)
